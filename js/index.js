f32f428bf45b72641320d9c707f1b6e27acb01f8
martin <mbroneid1@gmail.com>
2024-08-27 09:39:22 +0200

    Tredje commit, startet på styles

--- js/index.js
+++ js/index.js
@@ -1,9 +1,18 @@
-const background = document.querySelector(".body")
-background.style.setProperty("background-color", "lightblue", "important");
+const btn = document.querySelector(".kreft");
 
-const darkMode = (kaboom) =>{
+const darkMode = (kaboom) => {
+    const currentColor = getComputedStyle(document.documentElement).getPropertyValue('--background-color').trim();
+    
+    if (currentColor !== 'black') {
+        document.documentElement.style.setProperty('--background-color', 'black');
+        document.documentElement.style.setProperty('--text-color', 'white');
+    } else {
+        document.documentElement.style.setProperty('--background-color', 'white');
+        document.documentElement.style.setProperty('--text-color', 'black');
 
-}
+    }
+};
+
 const lightMode = (kaboom) =>{
 
 }
